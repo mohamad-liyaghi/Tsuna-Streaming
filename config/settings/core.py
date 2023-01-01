d51168db2a1832aa2211544c4b5c786feb7c9283
Ml06py <liaghimohamad69@gmail.com>
2023-01-01 12:34:59 +0330

    Rest framework installed and added to installed app

--- config/settings/core.py
+++ config/settings/core.py
@@ -16,6 +16,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third party 
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -86,3 +89,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+
+}
