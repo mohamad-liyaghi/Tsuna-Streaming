9920c15495b11d81680544afd1386e1e8b92a546
Ml06py <liaghimohamad69@gmail.com>
2023-01-01 12:31:01 +0330

    Django debug toolbar added

--- config/settings/development.py
+++ config/settings/development.py
@@ -4,6 +4,18 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+INSTALLED_APPS += ["debug_toolbar",]
+
+MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware",]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': lambda _request: DEBUG
+}
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -14,3 +26,4 @@ DATABASES = {
         'PORT': config("DB_PORT")
     }
 }
+
