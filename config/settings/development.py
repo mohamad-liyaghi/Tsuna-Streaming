c413d65e62d2b221d1b89369c28b980a8e838c92
Ml06py <liaghimohamad69@gmail.com>
2023-01-01 12:20:33 +0330

    Dev/Production settings added and docker-compose updated

---
+++ config/settings/development.py
@@ -0,0 +1,16 @@
+from .core import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config("DB_NAME"),
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASSWORD"),
+        'HOST': config("DB_HOST"),
+        'PORT': config("DB_PORT")
+    }
+}
