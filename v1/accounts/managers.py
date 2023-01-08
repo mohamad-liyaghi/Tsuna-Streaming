97e457b562d1e5d78585a0275e3c26e9f2f3f596
Ml06py <liaghimohamad69@gmail.com>
2023-01-08 13:18:58 +0330

    Default user_id and user_id generator added

--- v1/accounts/managers.py
+++ v1/accounts/managers.py
@@ -14,9 +14,8 @@ class AccountManager(BaseUserManager):
         except:
             kwargs["bio"] = "Hey there, i am using tsuna streaming."
 
-        user_id = random.randint(0, 999999999999999)
         is_active = False
-        user = self.model(email=email, user_id=user_id, is_active=is_active,
+        user = self.model(email=email, is_active=is_active,
                         role="n", **kwargs)
 
         user.set_password(password)
@@ -31,10 +30,8 @@ class AccountManager(BaseUserManager):
             kwargs["bio"]
         except:
             kwargs["bio"] = "Hey there, i am using tsuna streaming."
-
-        user_id = random.randint(0, 999999999999999)
         is_active = True
-        user = self.model(email=email, user_id=user_id, is_active=is_active, is_superuser=True, is_staff=True,
+        user = self.model(email=email, is_active=is_active, is_superuser=True, is_staff=True,
                         role="a", **kwargs)
 
         user.set_password(password)
--- v1/accounts/utils.py
+++ v1/accounts/utils.py
@@ -1,4 +1,7 @@
-import uuid
+import uuid, random
 
 def token_generator():
     return uuid.uuid4().hex
+
+def user_id_generator():
+    return random.randint(0, 999999999999999)
