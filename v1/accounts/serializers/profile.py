40c10d7960be42dd0d2fc459e0468a1430562363
Ml06py <liaghimohamad69@gmail.com>
2023-01-09 12:00:19 +0330

    Update profile added

--- v1/accounts/serializers/profile.py
+++ v1/accounts/serializers/profile.py
@@ -10,3 +10,8 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = USER
         fields = ["email", "first_name", "last_name", 
                 "picture", "bio", "get_role_display", "user_id"]
+                
+        extra_kwargs = {
+            'email': {'read_only': True},
+            'user_id': {'read_only': True},            
+        }
--- v1/accounts/views/profile.py
+++ v1/accounts/views/profile.py
@@ -2,7 +2,8 @@ from django.shortcuts import get_object_or_404
 from django.contrib.auth import get_user_model
 from rest_framework.generics import RetrieveUpdateAPIView
 from accounts.serializers.profile import ProfileSerializer
-
+from rest_framework.response import Response
+from rest_framework import status
 
 USER = get_user_model()
 
@@ -21,3 +22,20 @@ class ProfileView(RetrieveUpdateAPIView):
     def get(self, request, *args, **kwargs):
         '''Return given users information'''
         return super().get(request, *args, **kwargs)
+    
+    
+    def put(self, request, *args, **kwargs):
+        '''Update a user (only the profile owner can update)'''
+        if request.user ==  self.get_object():
+            return super().put(request, *args, **kwargs)
+
+        return Response("You can not update this page.", 
+                        status=status.HTTP_403_FORBIDDEN)
+    
+    def patch(self, request, *args, **kwargs):
+        '''Update a user (only the profile owner can update)'''
+        if request.user ==  self.get_object():
+            return super().patch(request, *args, **kwargs)
+
+        return Response("You can not update this page.", 
+                        status=status.HTTP_403_FORBIDDEN)
