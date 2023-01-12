22310bf625bdf834bb3bfd5166982204f34b601e
Ml06py <liaghimohamad69@gmail.com>
2023-01-12 11:41:49 +0330

    Add plan added

--- v1/accounts/serializers/subscription.py
+++ v1/accounts/serializers/subscription.py
@@ -6,4 +6,9 @@ class PlanListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Plan
-        fields = ["title", "description",    "price", "token", "active_months", "is_avaiable"]
+        fields = ["title", "description", "price", "active_months", "is_avaiable", "token",]
+
+        extra_kwargs = {
+            'description': {'write_only': True},       
+            'token': {"read_only" : True}
+        }
