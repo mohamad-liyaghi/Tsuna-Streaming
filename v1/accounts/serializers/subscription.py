2fb0df5f09e3116407fcdfb4ca86b25928f6896e
Ml06py <liaghimohamad69@gmail.com>
2023-01-12 11:46:57 +0330

    Plan detail page added

--- v1/accounts/serializers/subscription.py
+++ v1/accounts/serializers/subscription.py
@@ -12,3 +12,10 @@ class PlanListSerializer(serializers.ModelSerializer):
             'description': {'write_only': True},       
             'token': {"read_only" : True}
         }
+
+class PlanDetailSerializer(serializers.ModelSerializer):
+    lookup_field = "token"
+    
+    class Meta:
+        model = Plan
+        fields = ["title", "description", "price", "active_months", "is_avaiable", "token",]
--- v1/accounts/views/subscription.py
+++ v1/accounts/views/subscription.py
@@ -1,23 +1,27 @@
 from rest_framework.viewsets import ModelViewSet
 from accounts.models import Plan
 from accounts.permissions import AllowAdminPermission, AllowAuthenticatedPermission
-from accounts.serializers.subscription import PlanListSerializer
+from accounts.serializers.subscription import PlanListSerializer, PlanDetailSerializer
 
 
 class SubscriptionViewSet(ModelViewSet):
     '''A Viewset for Creating, retrieving, updating and deleting plans and buy subscriptions.'''
     queryset = Plan.objects.all().order_by("-is_avaiable", "active_months")
-    
+    lookup_field = "token"
+
     def get_serializer_class(self):
         '''Return the appropriate serializer'''
 
         if self.action in ["list", "create"]:
             return PlanListSerializer
+
+        elif self.action in ["retrieve", "update", "partial_update"]:
+            return PlanDetailSerializer
     
     def get_permissions(self):
         '''return the appropriate permission class'''
 
-        if self.action in ["list"]:
+        if self.action in ["list", "retrieve"]:
             permission_classes = [AllowAuthenticatedPermission]
 
         else:
