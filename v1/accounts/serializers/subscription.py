7374630debc608ddd6337f170c049ea63834db7f
Ml06py <liaghimohamad69@gmail.com>
2023-01-12 11:37:35 +0330

    Plan List added

--- v1/accounts/serializers/subscription.py
+++ v1/accounts/serializers/subscription.py
@@ -1,1 +1,9 @@
 from rest_framework import serializers
+from accounts.models import Plan
+
+
+class PlanListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Plan
+        fields = ["title", "description",    "price", "token", "active_months", "is_avaiable"]
--- v1/accounts/views/subscription.py
+++ v1/accounts/views/subscription.py
@@ -1,8 +1,27 @@
 from rest_framework.viewsets import ModelViewSet
 from accounts.models import Plan
 from accounts.permissions import AllowAdminPermission, AllowAuthenticatedPermission
+from accounts.serializers.subscription import PlanListSerializer
+
 
 class SubscriptionViewSet(ModelViewSet):
     '''A Viewset for Creating, retrieving, updating and deleting plans and buy subscriptions.'''
-    permission_classes = [AllowAdminPermission,]
-    queryset = Plan.objects.all()
+    queryset = Plan.objects.all().order_by("-is_avaiable", "active_months")
+    
+    def get_serializer_class(self):
+        '''Return the appropriate serializer'''
+
+        if self.action in ["list", "create"]:
+            return PlanListSerializer
+    
+    def get_permissions(self):
+        '''return the appropriate permission class'''
+
+        if self.action in ["list"]:
+            permission_classes = [AllowAuthenticatedPermission]
+
+        else:
+            permission_classes = [AllowAdminPermission]
+
+        return [permission() for permission in permission_classes]
+        
