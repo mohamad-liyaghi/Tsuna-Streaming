affbf9d2401649a533cb6f9a61be96d422bd6900
Ml06py <liaghimohamad69@gmail.com>
2023-01-13 12:14:52 +0330

    SubscriptionViewSet Docs added

--- v1/accounts/views/subscription.py
+++ v1/accounts/views/subscription.py
@@ -1,11 +1,34 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from drf_spectacular.utils import extend_schema, extend_schema_view
 from accounts.models import Plan
 from accounts.permissions import AllowAdminPermission, AllowAuthenticatedPermission
 from accounts.serializers.subscription import PlanListSerializer, PlanDetailSerializer, AvailabilitySerializer
 
-
+@extend_schema_view(
+    list=extend_schema(
+        description="List of subscription plans."
+    ),
+    create=extend_schema(
+        description="Create a new subscription plan [Admin only]."
+    ),
+    retrieve=extend_schema(
+        description="Detail page of a subscription plan."
+    ),
+    update=extend_schema(
+        description="Update a subscription plan [Admin only]."
+    ),
+    partial_update=extend_schema(
+        description="Update a subscription plan [Admin only]."
+    ),
+    destroy=extend_schema(
+        description="Delete a subscription plan [Admin only]."
+    ),
+    availability=extend_schema(
+        description="Change is_available status of a subscription plan [Admin only]."
+    ),
+)
 class SubscriptionViewSet(ModelViewSet):
     '''A Viewset for Creating, retrieving, updating and deleting plans and buy subscriptions.'''
     queryset = Plan.objects.all().order_by("-is_available", "active_months")
