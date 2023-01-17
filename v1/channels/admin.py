a5fbee7cf1389bec1dc51c55ce186d3f2663f78e
Ml06py <liaghimohamad69@gmail.com>
2023-01-17 12:55:06 +0330

    Channel admin added

--- v1/channels/admin.py
+++ v1/channels/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from channels.models import Channel
 
-admin.site.register(Channel)
+
+@admin.register(Channel)
+class ChannelAdmin(admin.ModelAdmin):
+    
+    list_display = ["title", "owner", "token", "is_verified"]
+
+    def has_change_permission(self, request, obj=None):
+        '''No body can change info in admin panel'''
+        return False
