f64201b50d53f27ffa47df3c753c2e7cc3c7cc7d
Ml06py <liaghimohamad69@gmail.com>
2023-01-27 12:44:38 +0330

    Video model added

--- v1/videos/admin.py
+++ v1/videos/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from videos.models import Video
 
-# Register your models here.
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ["title", "channel", "user", "is_published"]
--- v1/videos/models.py
+++ v1/videos/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.conf import settings
+from channels.models import Channel
 
-# Create your models here.
+
+class Video(models.Model):
+
+    class Visibility(models.TextChoices):
+        PRIVATE = ("pr", "Private")
+        PUBLISHED = ("pu", "Public")
+
+    title = models.CharField( max_length=100)
+    description = models.TextField(max_length=300)
+
+    video = models.FileField(upload_to="videos/user_video/")
+    thumbnail = models.ImageField(upload_to="videos/thumbnail/", default="media/default-video-thumbnail.jpg")
+
+    token = models.CharField(max_length=32)
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="videos")
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE, related_name="videos")   
+    date = models.DateTimeField(auto_now_add=True)
+
+    visibility = models.CharField(max_length=2, choices=Visibility.choices, default=Visibility.PRIVATE)
+    is_updated = models.BooleanField(default=False)
+
+
+    @property
+    def is_published(self):
+        return self.visibility == "pu"
