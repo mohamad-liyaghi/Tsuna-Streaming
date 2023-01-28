380a150c03eef1bad888da75f323069aa78367e7
Ml06py <liaghimohamad69@gmail.com>
2023-01-28 14:27:10 +0330

    Video viewset added

--- v1/videos/urls.py
+++ v1/videos/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
+from videos.views import VideoViewSet
 
 app_name = "v1_videos"
+router = SimpleRouter()
 
-urlpatterns = []
+router.register("video",VideoViewSet, basename="video")
+
+urlpatterns = [
+    
+]
+
+urlpatterns += router.urls
--- v1/videos/views.py
+++ v1/videos/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from drf_spectacular.utils import extend_schema, extend_schema_view
 
-# Create your views here.
+
+@extend_schema_view()
+class VideoViewSet(ModelViewSet):
+    '''A viewset for adding, updating and retrieving videos'''
+    pass
