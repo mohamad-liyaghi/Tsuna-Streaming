8b4511f7ba28e4af25d21a9f358e2408ea269fe3
Ml06py <liaghimohamad69@gmail.com>
2023-01-29 16:45:22 +0330

    Video Permission added

--- v1/videos/views.py
+++ v1/videos/views.py
@@ -1,7 +1,10 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from drf_spectacular.utils import extend_schema, extend_schema_view
 from videos.models import Video
+from channels.models import ChannelAdmin
+from videos.permissions import VideoPermission
 from videos.serializers import VideoListSerializer, VideoCreateSeriaizer, VideoDetailSerializer
 
 
@@ -25,9 +28,15 @@ from videos.serializers import VideoListSerializer, VideoCreateSeriaizer, VideoD
 )
 class VideoViewSet(ModelViewSet):
     '''A viewset for adding, updating and retrieving videos'''
-
-    permission_classes = [IsAuthenticated,]
     lookup_field = "token"
+
+    def get_permissions(self):
+        if self.action in ["retrieve", "update", "partial_update", "destroy"]:
+            permission_classes = [IsAuthenticated, VideoPermission]
+        else:
+            permission_classes = [IsAuthenticated]
+
+        return [permission() for permission in permission_classes]
 
     def get_serializer_context(self):
         return {'user' : self.request.user}
@@ -49,9 +58,15 @@ class VideoViewSet(ModelViewSet):
         return Video.objects.select_related('user', 'channel')\
             .filter(user=self.request.user).order_by("-date")
     
+    def get_object(self):
+        video = get_object_or_404(Video.objects.select_related("channel"), 
+                                    token=self.kwargs["token"])
+        self.check_object_permissions(self.request, video)
+        return video
+    
     def update(self, request, *args, **kwargs):
         # set is_updated to True after updating the video
         object = self.get_object()
         object.is_updated = True        
         object.save()
-        return super().update(request, *args, **kwargs)
+        return super().update(request, *args, **kwargs)        
