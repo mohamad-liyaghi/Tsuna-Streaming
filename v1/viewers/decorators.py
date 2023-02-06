d5175e415850e85dc778711319c111f1fc700d66
Ml06py <liaghimohamad69@gmail.com>
2023-02-06 20:08:33 +0330

    Viewer decorator optimized

--- v1/viewers/decorators.py
+++ v1/viewers/decorators.py
@@ -1,5 +1,3 @@
-from django.shortcuts import get_object_or_404
-from django.contrib.contenttypes.models import ContentType
 from viewers.models import Viewer
 
 def check_viewer_status(view):
@@ -7,16 +5,16 @@ def check_viewer_status(view):
     
     def check_view_exists_or_not(self, request, *args, **kwargs):        
         object = self.get_object()
-        content_type_model = get_object_or_404(ContentType, model=str(object.__class__.__name__).lower())
 
         # check is there a view for the object
-        viewer = Viewer.objects.filter(content_type=content_type_model, object_id=object.id)
+        viewer = object.viewer.filter(user=request.user)
 
         if viewer:
             return view(self, request, *args, **kwargs)
 
         # create a user view for an object if does not exist            
-        Viewer.objects.create(user=request.user, content_type=content_type_model, object_id=object.id)
+        Viewer.objects.create(user=request.user, content_object=object)
+
         return view(self, request, *args, **kwargs)
 
     return check_view_exists_or_not
