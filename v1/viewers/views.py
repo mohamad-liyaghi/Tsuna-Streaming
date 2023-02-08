ddc6710b191e864c21186b9e4032a2e99b06b6b3
Ml06py <liaghimohamad69@gmail.com>
2023-02-08 16:17:40 +0330

    Viewer list cached

--- v1/viewers/views.py
+++ v1/viewers/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import get_object_or_404
 from django.contrib.contenttypes.models import ContentType
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from drf_spectacular.utils import extend_schema, extend_schema_view
 from viewers.serializers import ViewerListSerializer
-
 
 @extend_schema_view(
     get=extend_schema(
@@ -17,6 +18,7 @@ class ViewerListView(APIView):
     '''List of an objects viewers'''
     permission_classes = [IsAuthenticated,]
 
+    @method_decorator(cache_page(4))
     def get(self, request, content_type_id, token, *args, **kwargs):
         content_type_model = get_object_or_404(ContentType, id=content_type_id)
         object = get_object_or_404(content_type_model.model_class(), token=token)
