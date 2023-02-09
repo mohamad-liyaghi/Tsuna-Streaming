a08fd7b8ebf72dda01e5dc0b042582179d5b1e9a
Ml06py <liaghimohamad69@gmail.com>
2023-02-09 13:06:45 +0330

    user_id_generator updated

--- v1/accounts/utils.py
+++ v1/accounts/utils.py
@@ -1,7 +1,18 @@
+from django.contrib.contenttypes.models import ContentType
 import uuid, random
 
 def token_generator():
     return uuid.uuid4().hex
 
 def user_id_generator():
-    return random.randint(0, 999999999999999)
+    '''Create a unique user id for accounts'''
+    # get the accounts model 
+    Account = ContentType.objects.get(app_label='accounts', model="account").model_class()
+    
+    user_id = random.randint(0, 999999999999999)
+
+    # if user with generated userid exists, function get calls again.
+    if Account.objects.filter(user_id=user_id):
+        user_id_generator()
+
+    return user_id
