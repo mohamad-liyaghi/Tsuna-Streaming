4f5c127cb00ac9572e871ece03abc9b880c03f38
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2023-03-02 20:51:18 +0330

    Comments tests added

--- v1/comments/tests.py
+++ v1/comments/tests.py
@@ -2,7 +2,7 @@ from channels.models import Channel
 from videos.models import Video
 from accounts.models import Account
 from comments.models import Comment
-
+from comments.exceptions import CommentNotAllowed
 import pytest
 
 
@@ -29,3 +29,28 @@ class TestCommentModel:
         self.video.delete()
         assert Comment.objects.count() == 0
         assert Comment.objects.count() != 1
+
+
+    def test_raise_exception_comment_not_allowed(self):
+        '''raise exception when allow_comment is False and user wants to add comment'''
+
+        self.create_video()
+        # create a comment when allow_comment is True
+        Comment.objects.create(content_object=self.video, user=self.user, body='test')
+
+        # disallow comments
+        self.video.allow_comment = False
+
+        with pytest.raises(CommentNotAllowed):
+    
+            Comment.objects.create(content_object=self.video, user=self.user, body='test')
+
+
+    def test_update_comment(self):
+        '''When user update a comment, the edited field gets updated to True'''
+        self.create_video()
+        comment = Comment.objects.create(content_object=self.video, user=self.user, body='testd')
+        
+        comment.body='test'
+        comment.save()
+        assert comment.edited == True
