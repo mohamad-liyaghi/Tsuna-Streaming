e2aed267719afcecb130102f390f50a68b95c58a
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2023-03-02 20:18:37 +0330

    Common fields moved to BaseContentModel

--- v1/core/models.py
+++ v1/core/models.py
@@ -7,10 +7,45 @@ from core.exceptions import AdminNotFound, ObjectPermissionDenied
 class BaseContentModel(models.Model):
     '''
         The base content model of content models (video, broadcast etc...).
+        This model contains some common fields.
         The only diffrence between this model and models.Model is the methods.
-        get_model_content_type_id -> return models Content type id for vote and comment stuff.
-        get_viewer_count -> return views of an object (eg: video).
     '''
+
+    class Visibility(models.TextChoices):
+        PRIVATE = ("pr", "Private")
+        PUBLISHED = ("pu", "Public")
+
+    visibility = models.CharField(max_length=2, choices=Visibility.choices, default=Visibility.PRIVATE)
+    is_updated = models.BooleanField(default=False)
+
+    # whether or not user can add comment
+    allow_comment = models.BooleanField(default=True)
+
+    date = models.DateTimeField(auto_now_add=True)
+
+
+    @cached_property_with_ttl(ttl=60 * 60 * 24)
+    def get_model_content_type_id(self):
+        '''Return content type id of the model (Eg: Video)'''
+
+        cls = self.__class__
+        return ContentType.objects.get_for_model(cls).id if cls else None
+
+
+    @cached_property_with_ttl(ttl=60 * 60 * 24)
+    def get_model_content_type(self):
+        '''Return content type instance of a model (Eg: Video)'''
+
+        cls = self.__class__
+        return ContentType.objects.get_for_model(cls) if cls else None
+
+
+    @cached_property_with_ttl(ttl=60)
+    def get_viewer_count(self):    
+        '''Return objects viewers cout'''
+
+        return self.viewer.count()
+
 
     class Meta:
         abstract = True
@@ -37,22 +72,4 @@ class BaseContentModel(models.Model):
 
         return super(BaseContentModel, self).save(*args, **kwargs)
 
-    @cached_property_with_ttl(ttl=60 * 60 * 24)
-    def get_model_content_type_id(self):
-        '''Return content type id of a model'''
 
-        cls = self.__class__
-        return ContentType.objects.get_for_model(cls).id if cls else None
-
-    @cached_property_with_ttl(ttl=60 * 60 * 24)
-    def get_model_content_type(self):
-        '''Return content type instance of a model'''
-
-        cls = self.__class__
-        return ContentType.objects.get_for_model(cls) if cls else None
-
-    @cached_property_with_ttl(ttl=60)
-    def get_viewer_count(self):    
-        '''Return objects views '''
-
-        return self.viewer.count()
--- v1/videos/models.py
+++ v1/videos/models.py
@@ -12,10 +12,6 @@ from viewers.models import Viewer
 
 class Video(BaseContentModel):
 
-    class Visibility(models.TextChoices):
-        PRIVATE = ("pr", "Private")
-        PUBLISHED = ("pu", "Public")
-
     title = models.CharField( max_length=100)
     description = models.TextField(max_length=300)
 
@@ -26,15 +22,9 @@ class Video(BaseContentModel):
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="videos")
     channel = models.ForeignKey(Channel, on_delete=models.CASCADE, related_name="videos")   
-    date = models.DateTimeField(auto_now_add=True)
-
-    visibility = models.CharField(max_length=2, choices=Visibility.choices, default=Visibility.PRIVATE)
-    is_updated = models.BooleanField(default=False)
-    
-    # whether or not user can add comment
-    allow_comment = models.BooleanField(default=True)
 
     objects = VideoManager()
+    
     vote = GenericRelation(Vote)
     comment = GenericRelation(Comment)
     viewer = GenericRelation(Viewer)
