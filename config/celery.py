da6751afc268c40b69c3a4e545adaf9f889c473c
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2023-03-06 17:29:01 +0330

    TASK_ALWAYS_EAGER env variable added to  pytest configs

--- config/celery.py
+++ config/celery.py
@@ -1,8 +1,15 @@
-import os
 from celery import Celery
+import os
 
 
 celery = Celery("config")
+
+
+# Pytest sets TASK_ALWAYS_EAGER env variable for testing purposes.
+if os.environ.get('TASK_ALWAYS_EAGER'):
+    # while testing, tasks wont be executed on workers.
+    celery.conf.task_always_eager = True
+
 
 celery.config_from_object("django.conf:settings", namespace="CELERY")
 celery.autodiscover_tasks()
