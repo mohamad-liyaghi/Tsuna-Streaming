43ef24c6fe4920a58f34b7b34e05ee2cf1d15136
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2023-03-13 15:49:39 +0330

    Subscription set finish_date added in create method

--- v1/memberships/models/subscription.py
+++ v1/memberships/models/subscription.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 from django.utils import timezone
 from memberships.models import Membership
+import datetime
 
 
 class Subscription(models.Model):
@@ -17,14 +18,33 @@ class Subscription(models.Model):
     token = models.CharField(max_length=32, null=True, blank=True)
 
 
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user'],
+                name="unique_subscription"
+            )
+        ]
+
+
     def save(self, *args, **kwargs ):
         '''Change user status after creating a subscription'''
 
         if not self.pk:
 
+            # If this is a new subscription, change the user's role to 'p' (Premium)
             if not self.user.role == 'a':
                 self.user.role = 'p'
                 self.user.save()
+
+            # Get the Membership's active months
+            membership_active_months = self.membership.active_months
+
+            # Calculate the time that the subscription should expire
+            finish_date = timezone.now() + datetime.timedelta(membership_active_months * 30)
+
+            # set the expiration time
+            self.finish_date = finish_date
 
         return super(Subscription, self).save(*args, **kwargs)
 
@@ -32,6 +52,7 @@ class Subscription(models.Model):
     def delete(self, *args, **kwargs):
         '''Change user status after deleting a subscription'''
         
+        # When a subscription is deleted, change the user's role back to 'n' (normal)
         if not self.user.role == 'a':
             self.user.role = 'n'
             self.user.save()
