0b248f50d45ad749d9ff5872561942cfb23dea01
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2023-03-14 15:56:50 +0330

    Notify user after deleting subscription signal  added

--- v1/memberships/signals.py
+++ v1/memberships/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import pre_save, post_save
+from django.db.models.signals import pre_save, post_save, pre_delete
 from django.dispatch import receiver
 from memberships.models import Membership, Subscription
 from v1.core.receivers import create_token_after_creating_object
@@ -18,3 +18,16 @@ def notify_user_subscription(sender, **kwargs):
 
         send_email.delay(template_name="emails/notify_premium.html", email=user.email, first_name=user.first_name, 
                                 plan=subscription.membership.title, finish_date=subscription.finish_date)
+
+
+@receiver(pre_delete, sender=Subscription)
+def notify_after_deleting_subscription(sender, **kwargs):
+    '''Notify after deleting subscription'''
+
+    subscription = kwargs["instance"]
+
+    send_email.delay(template_name="emails/notify_unsubscribed_user.html", first_name=subscription.user.first_name,
+                                email=subscription.user.email,
+                                plan=subscription.membership.title, 
+                                start_date=subscription.start_date)
+
