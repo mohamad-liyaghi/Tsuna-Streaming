1addd1b322aae4fcd9db1a49dab7596dfb5f44db
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2023-03-17 16:05:59 +0330

    get_subscriber method added to ChannelSubscriber model

--- v1/channel_subscribers/models.py
+++ v1/channel_subscribers/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.core.cache import cache
 from channels.models import Channel
+from accounts.models import Account
 from django.conf import settings
 
 
@@ -23,3 +25,35 @@ class ChannelSubscriber(models.Model):
 
     def __str__(self) -> str:
         return str(self.user)
+    
+
+    @classmethod
+    def get_subscriber(cls, channel_token, user_token):
+        '''Get subscriber from cache and return it. if not exist in cache, get from database.'''
+
+        key = f'subscriber:{channel_token}:{user_token}'
+        # Get subscriber from cache
+        cache_subscriber = cache.get(key)
+
+        # if Subscriber exists in cache
+        if cache_subscriber:
+            # If status of the subscriber in cache is subscribed means user has subscribed
+            # If status of subscriber is unsubscribed, means user has unsubscribed
+            if cache_subscriber.get('status', None) == 'subscribed':
+                return cache_subscriber
+            
+            return None
+        
+        # if subscriber is not in cache
+        else:  
+                # Get subscriber from database
+                subscriber = cls.objects.filter(user__token=user_token, channel__token=channel_token).exists()
+                # if subscriber exists in cache
+                if subscriber:
+                    # set subscriber in cache
+                    cache.set(key=key, value={'status' : 'subscribed'}, timeout=60 * 60 * 24)
+                    # return subscriber
+                    return cache.get(key)
+                
+                return None
+            
