d6e11385a1946142aaae1d60c8edfcb85e9cf03c
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2023-03-18 09:39:06 +0330

    create_subscriber method added to ChannelSubscriber model

--- v1/channel_subscribers/models.py
+++ v1/channel_subscribers/models.py
@@ -39,7 +39,7 @@ class ChannelSubscriber(models.Model):
         if cache_subscriber:
             # If status of the subscriber in cache is subscribed means user has subscribed
             # If status of subscriber is unsubscribed, means user has unsubscribed
-            if cache_subscriber.get('status', None) == 'subscribed':
+            if cache_subscriber.get('subscription_status', None) == 'subscribed':
                 return cache_subscriber
             
             return None
@@ -51,9 +51,65 @@ class ChannelSubscriber(models.Model):
                 # if subscriber exists in cache
                 if subscriber:
                     # set subscriber in cache
-                    cache.set(key=key, value={'status' : 'subscribed'}, timeout=60 * 60 * 24)
+                    cache.set(key=key, value={'subscription_status' : 'subscribed', 'source' : 'database'}, timeout=60 * 60 * 24)
                     # return subscriber
                     return cache.get(key)
                 
                 return None
+        
+
+    @classmethod
+    def create_subscriber(cls, channel_token, user_token):
+        '''
+            If user has subscribed a channel, it sets in cache and return None
+            otherwise it sets the subscription in cache and return the info
+        '''
+        
+        key = f'subscriber:{channel_token}:{user_token}'
+
+        # Get the user from cache
+        cache_subscriber = cache.get(key)
+
+        # If user exists in cache
+        if cache_subscriber:
+
+            # If user is in cache and already subscribed the channel, it returns None
+            if cache_subscriber.get('subscription_status', None) == 'subscribed':
+                return None
+            
+            # If subscription_status == 'ubsubscribed' means that user has unsubscribed and not wants to subscribe again
+            cache_subscriber['subscription_status'] = 'subscribed'
+            
+
+            
+        # If subscriber is not in cache and it is in db it sets the user in cache and return None
+        # Cuz user has Subscribed the channel
+        elif cls.objects.filter(user__token=user_token, channel__token=channel_token).exists():
+            
+            # Set the subscriber in cache for subsequent requests
+            cache.set(
+                key=key,
+                value={
+                    'subscription_status': 'subscribed',
+                    'source': 'database'
+                },
+                timeout=60 * 60 * 24
+            )
+
+            return None
+
+        # If subscriber not exists in cache and also in DB
+        # It sets the subscriber in cache and after that celery inserts them into db
+        else:
+            cache.set(
+                key=key,
+                value={
+                    'subscription_status' : 'subscribed', 
+                    'source' : 'cache'
+                }, 
+                timeout=60 * 60 * 24
+                )
+            return cache.get(key)
+
+    
             
