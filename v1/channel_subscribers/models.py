482b89be39ab47c660837176a209735487ba17bb
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2023-03-19 12:00:11 +0330

    delete_subscriber method added to ChannelSubscriber model

--- v1/channel_subscribers/models.py
+++ v1/channel_subscribers/models.py
@@ -1,15 +1,15 @@
 from django.db import models
+from django.utils import timezone
 from django.core.cache import cache
 from channels.models import Channel
 from accounts.models import Account
-from django.conf import settings
 
 
 class ChannelSubscriber(models.Model):
     """Model to represent a user's subscription to a channel."""
     
     channel = models.ForeignKey(Channel, on_delete=models.CASCADE, related_name="subscribers")
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+    user = models.ForeignKey(Account, on_delete=models.CASCADE,
                              related_name="subscribed_channels")
 
     date = models.DateTimeField(auto_now_add=True)
@@ -51,7 +51,14 @@ class ChannelSubscriber(models.Model):
                 # if subscriber exists in cache
                 if subscriber:
                     # set subscriber in cache
-                    cache.set(key=key, value={'subscription_status' : 'subscribed', 'source' : 'database'}, timeout=60 * 60 * 24)
+                    cache.set(
+                        key=key, 
+                        value={
+                            'subscription_status' : 'subscribed', 
+                            'source' : 'database'
+                        },
+                        timeout=60 * 60 * 24)
+                    
                     # return subscriber
                     return cache.get(key)
                 
@@ -73,19 +80,27 @@ class ChannelSubscriber(models.Model):
         # If user exists in cache
         if cache_subscriber:
 
-            # If user is in cache and already subscribed the channel, it returns None
-            if cache_subscriber.get('subscription_status', None) == 'subscribed':
-                return None
-            
             # If subscription_status == 'ubsubscribed' means that user has unsubscribed and not wants to subscribe again
-            cache_subscriber['subscription_status'] = 'subscribed'
+            if cache_subscriber.get('subscription_status', None) == 'unsubscribed':
+                    cache.set(
+                    key=key,
+                    value={
+                        'subscription_status': 'subscribed',
+                        'source': 'cache',
+                        'date' : timezone.now(),
+                    },
+                    timeout=60 * 60 * 24)
+                    return cache.get(key)
+            
+            # If user is in cache and already subscribed the channel, it returns None
+            return None
             
 
             
         # If subscriber is not in cache and it is in db it sets the user in cache and return None
         # Cuz user has Subscribed the channel
         elif cls.objects.filter(user__token=user_token, channel__token=channel_token).exists():
-            
+
             # Set the subscriber in cache for subsequent requests
             cache.set(
                 key=key,
@@ -101,15 +116,66 @@ class ChannelSubscriber(models.Model):
         # If subscriber not exists in cache and also in DB
         # It sets the subscriber in cache and after that celery inserts them into db
         else:
-            cache.set(
+            user = Account.objects.filter(token=user_token).exists()
+            channel = Channel.objects.filter(token=channel_token).exists()
+            # If user and channel are valid set a cache
+            if user and channel:
+                cache.set(
+                    key=key,
+                    value={
+                        'subscription_status' : 'subscribed', 
+                        'source' : 'cache'
+                    }, 
+                    timeout=60 * 60 * 24
+                    )
+                return cache.get(key)
+            
+            return None
+
+    
+    @classmethod
+    def delete_subscriber(cls, channel_token, user_token):
+        key = f'subscriber:{channel_token}:{user_token}'
+
+        # Get the subscriber from the cache
+        cache_subscriber = cache.get(key)
+
+        # If subscriber is in cache
+        if cache_subscriber:
+
+            # If user has already subscribed the channel and exists in cache
+            # It will update the cache that user has unsubscribed
+            # Then celery will delete the sub
+            if cache_subscriber.get('subscription_status', None) == 'subscribed':
+                
+                # Update the cache
+                cache.set(
                 key=key,
                 value={
-                    'subscription_status' : 'subscribed', 
+                    'subscription_status' : 'unsubscribed', 
                     'source' : 'cache'
                 }, 
                 timeout=60 * 60 * 24
                 )
-            return cache.get(key)
-
-    
+                return cache.get(key)
             
+            # if subscription_status is unsubscribed
+            return None
+        
+        # If sub is not in cache but in db
+        elif cls.objects.filter(user__token=user_token, channel__token=channel_token).exists():
+            
+            # Set a cache that user has unsubscribed
+            cache.set(
+                key=key,
+                value={
+                    'subscription_status': 'unsubscribed',
+                    'source': 'cache'
+                },
+                timeout=60 * 60 * 24
+            )
+
+            return cache.get(key)
+        
+        # If not in cache not in db it returns None
+        return None
