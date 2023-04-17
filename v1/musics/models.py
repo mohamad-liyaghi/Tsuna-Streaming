609aff01a6d41606c62da1d7624a11a063610709
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2023-04-17 16:51:33 +0330

    Music model tests added

--- v1/musics/models.py
+++ v1/musics/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.core.exceptions import ValidationError  
-from core.models import BaseContentModel
+from v1.core.models import BaseContentModel
 from channels.models import Channel
 
 
