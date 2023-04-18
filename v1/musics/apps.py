acb38aa7e25b749017fe05ba60212a19c94fe420
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2023-04-18 15:46:06 +0330

    Music creation notify signal added

--- v1/musics/apps.py
+++ v1/musics/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class MusicsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'musics'
+
+    def ready(self) -> None:
+        import musics.signals
--- v1/musics/urls.py
+++ v1/musics/urls.py
@@ -5,5 +5,5 @@ app_name = 'v1_musics'
 
 urlpatterns = [
     path('<str:channel_token>/', MusicListCreateView.as_view(), name='music_list_create'),
-    path('<str:channel_token>/<str:music_token>', MusicDetailView.as_view(), name='music_detail'),
+    path('<str:channel_token>/<str:music_token>/', MusicDetailView.as_view(), name='music_detail'),
 ]
