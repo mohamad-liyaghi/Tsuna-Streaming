24f4d04ab1758ea83476cb9d7455ac4e389eeacb
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2023-07-21 13:43:23 +0330

    Subscriber celery tasks updated

--- apps/channel_subscribers/tasks.py
+++ apps/channel_subscribers/tasks.py
@@ -1,60 +1,83 @@
 from django.core.cache import cache
+from django.db.models import Q
 from celery import shared_task
 from channel_subscribers.models import ChannelSubscriber
-from accounts.models import Account
-from channel_subscribers.models import Channel
+
+
+CACHE_SUBSCRIBER_KEY = 'subscribers:*:*'
 
 
 @shared_task
 def insert_subscriber_from_cache_into_db():
-    '''Insert subscribers that are in cache'''
-    subscriber_keys = cache.keys("subscriber:*:*")
+    """
+    Get all subscribers from cache and insert them into db
+    """
+    subscriber_keys = cache.keys(CACHE_SUBSCRIBER_KEY)
+    # Get all subscribers from cache
+    subscribers_in_cache = [
+        cache.get(key) for key in subscriber_keys
+    ]
 
-    if subscriber_keys:
-        for key in subscriber_keys:
-            subscriber = cache.get(key)
-            _, channel_token, user_token = key.split(':') # subscriber:channel_token:user_token
-            
-            if (subscriber and subscriber.get('subscription_status', '') == 'subscribed' 
-                    and subscriber.get('source', '') == 'cache'):
-                try:
-                    # Create channel subscriber
-                    ChannelSubscriber.objects.create(
-                        channel = Channel.objects.get(token=channel_token), 
-                        user = Account.objects.get(token=user_token),
-                        date = subscriber.get('date'),
-                    )
-                    subscriber['source'] = 'database'
-                    cache.set(key, subscriber)
+    # Filter records with source of `cache` and status of `subscribed`
+    cached_subscriber = filter(
+        lambda subscriber: (
+                subscriber.get('source') == 'cache'
+                and
+                subscriber.get('subscription_status') == 'subscribed'
+        ),
+        subscribers_in_cache
+    )
 
-                except:
-                    # Delete subscriber if operation failed
-                    cache.delete(key)
+    if cached_subscriber:
+        # Bulk create subcribers and insert to db
+        ChannelSubscriber.objects.bulk_create(
+            [
+                ChannelSubscriber(
+                    channel=subscriber.get('channel'),
+                    user=subscriber.get('user'),
+                    date=subscriber.get('date'),
+                ) for subscriber in cached_subscriber
+            ]
+        )
+    # Remove subscribers from cache
+    cache.delete_many(subscriber_keys)
 
 
 @shared_task
 def delete_unsubscribed_from_db():
-    '''Delete users that has unsubscribed a channel from db'''
+    """
+    Delete subscribers from db which
+    their status is unsubscribed and source is cache
+    """
 
-    subscriber_keys = cache.keys("subscriber:*:*")
+    subscriber_keys = cache.keys(CACHE_SUBSCRIBER_KEY)
+    # Retrieve all subscribers from cache
+    subscribers_in_cache = [
+        cache.get(key) for key in subscriber_keys
+    ]
 
-    if subscriber_keys:
-        for key in subscriber_keys:
-            subscriber = cache.get(key)
-            _, channel_token, user_token = key.split(':') # subscriber:channel_token:user_token
-            
-            # If subscriber status is unsubscribed and source is in cache
-            # Get and delete sub
-            if (subscriber and subscriber.get('subscription_status', '') == 'unsubscribed' 
-                    and subscriber.get('source', '') == 'cache'):
-                try:
-                    # Create channel subscriber
-                    ChannelSubscriber.objects.get(
-                        channel__token = channel_token,
-                        user__token = user_token,
-                    ).delete()
+    # Filter records with status of `unsubscribed`
+    cached_unsubscriber = filter(
+        lambda subscriber: (
+                subscriber.get('subscription_status') == 'unsubscribed'
+        ),
+        subscribers_in_cache
+    )
 
-                except:
-                    pass
+    if cached_unsubscriber:
+        # Zip the result (user, channel)
+        zipped_user_channel = [
+            (subscriber['user'], subscriber['channel'])
+            for subscriber in cached_unsubscriber
+        ]
+        # Get users and channels
+        users = [user for user, _ in zipped_user_channel]
+        channels = [channel for _, channel in zipped_user_channel]
 
-                cache.delete(key)
+        # Delete unsubscribers from db
+        ChannelSubscriber.objects.filter(
+            Q(user__in=users) & Q(channel__in=channels)
+        ).delete()
+
+    # Remove unsubscribers from cache
+    cache.delete_many(subscriber_keys)
