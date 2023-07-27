e8633b7cad62260e8a1bdc3298c06fde989fcfb0
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2023-07-27 14:39:27 +0330

    Cache Service updated

--- apps/core/services.py
+++ apps/core/services.py
@@ -10,9 +10,12 @@ from core.utils import ObjectSource
 class CacheService:
     """
     A service which provides methods for working with cache.
-    List/Get/Set/Update/Delete objects in cache.
+    List/Get/Set/Delete objects in cache.
     """
     def __init__(self, model: models.Model):
+        """
+        Set the default model for the service
+        """
         self.model = model
 
     def get_list(
@@ -20,54 +23,38 @@ class CacheService:
             key: str,
             channel: Channel,
     ) -> Union[list, int]:
-        # TODO: Clean code and add cache
         """
         Return List of objects from cache and db
         Args:
             key: str
             channel: Channel
         """
-        # Objects from cache
-        objects_in_cache = [
-            cache.get(object_key) for object_key in cache.keys(key)
-        ]
+        cached_result = cache.get(key)
 
-        # Filter cache objects with source of cache and not pending delete
-        cache_objects = list(
-            filter(
-                lambda obj: obj.get('source') == ObjectSource.CACHE.value,
-                objects_in_cache
+        if cached_result is None:
+            cache_objects = self.__get_all_from_cache(
+                key=f"cached_{key}",
+                object_key=key,
+                channel=channel
             )
-        )
+            db_objects = self.__get_all_from_db(key=f"db_{key}", channel=channel)
 
-        # Objects from db
-        db_objects = list(
-            self.model.objects.filter(channel=channel).values(
-                'user', 'channel', 'date'
-            )
-        )
+            # Combine cache and db objects
+            combined_objects = cache_objects + db_objects
 
-        # Combine cache and db objects
-        combined_objects = cache_objects + db_objects
+            unique_objects = self.__get_unique_objects(objects=combined_objects)
 
-        # Remove duplicate users and check pending delete
-        unique_objects = []
-        seen_users = set()
-        for obj in combined_objects:
-            user = obj.get('user')
-            if user not in seen_users:
-                if obj.get('pending_delete') is not True:
-                    unique_objects.append(obj)
-                seen_users.add(user)
-        # Return the unique objects
-        return unique_objects
+            cache.set(key, unique_objects, timeout=60)
+            # Return the unique objects
+            return unique_objects
+
+        return cached_result
 
     def get_from_cache(
             self, *,
             key: str,
             channel: Channel,
             user: settings.AUTH_USER_MODEL,
-            exists: bool = False
     ) -> Union[dict, None]:
         """
         Return object from cache or db or check if object exists in cache or db
@@ -75,29 +62,23 @@ class CacheService:
             key: str
             channel: Channel
             user: settings.AUTH_USER_MODEL
-            exists: bool - if True, check if object exists in cache or db
         """
-        # Search in db for object
-        object_in_db = self.__search_in_db(key=key, channel=channel, user=user)
+        # First search in db for the object
+        search_in_db = self.__search_in_db(
+            key=f"db_{key}", channel=channel, user=user
+        )
         # Search in cache for object
-        object_in_cache = self.__search_in_cache(key=key, channel=channel, user=user)
+        search_in_cache = self.__search_in_cache(key=key)
 
-        if object_in_cache and object_in_cache.get('pending_delete'):
-            return
-
-        # If user wants to check if object exists in cache or db, return True or False
-        if exists:
-            return object_in_db or object_in_cache
-
-        # If object is in DB, return it
-        if object_in_db:
-            return object_in_db
+        # If object is pending delete, return None
+        if search_in_cache and search_in_cache.get('pending_delete'):
+                return
 
         # If object is in cache, return it
-        if object_in_cache:
-            return object_in_cache
+        if search_in_db:
+            return search_in_db
 
-        return
+        return search_in_cache
 
     def create_cache(
             self, *,
@@ -117,7 +98,7 @@ class CacheService:
 
         # First check if object exists in cache or db
         # If exists, return None
-        if self.get_from_cache(key=key, channel=channel, user=user, exists=True):
+        if self.get_from_cache(key=key, channel=channel, user=user):
             return
 
         # If not exist, create a new object in cache
@@ -141,12 +122,14 @@ class CacheService:
             channel: Channel
             user: settings.AUTH_USER_MODEL
         """
-        get_cache = self.get_from_cache(key=key, channel=channel, user=user)
         # First check if object exists in cache or db
-        # If not exists, return None
+        get_cache = self.get_from_cache(key=key, channel=channel, user=user)
+        # If not exists, Raise exception
         if not get_cache:
             raise ValueError("Object does not exist in cache or db.")
 
+        # If object is in db, do not delete the cache
+        # Create a record which then celery would delete its object
         if get_cache.get('source') == ObjectSource.DATABASE.value:
             self.__set_cache(
                 key=key,
@@ -163,20 +146,11 @@ class CacheService:
     def __search_in_cache(
             self,
             key: str,
-            user: settings.AUTH_USER_MODEL,
-            channel: Channel
     ) -> Union[list, None]:
         """
         Search for object in cache
         """
-        # Get object from cache
-        cache_object = cache.get(key)
-
-        # If object exists in cache, return it
-        if cache_object is not None:
-            return cache_object
-        # Otherwise, return None
-        return
+        return cache.get(key)
 
     def __search_in_db(
             self,
@@ -238,3 +212,65 @@ class CacheService:
             timeout=60
         )
         return cache.get(key)
+
+    def __get_all_from_cache(
+            self, *,
+            key: str,
+            object_key: str,
+            channel: Channel,
+    ) -> list:
+        """
+        Return all objects that are only saved in cache
+        """
+        cached_result = cache.get(key)
+        if cached_result is None:
+            # Get all objects in cache
+            caches = [
+                cache.get(object_key) for object_key in cache.keys(object_key)
+            ]
+
+            # Filter objects that are only in cache, not saved in db
+            cached_object = list(
+                filter(
+                    lambda obj: obj.get('source') == ObjectSource.CACHE.value,
+                    caches
+                )
+            )
+            cache.set(key, cached_object, timeout=60)
+            return cached_object
+
+        return cached_result
+
+    def __get_all_from_db(
+            self, *,
+            key: str,
+            channel: Channel,
+    ) -> list:
+        """
+        Return all objects that are only saved in db
+        """
+        cached_result = cache.get(key)
+
+        if cached_result is None:
+            # Objects from db
+            db_objects = list(
+                self.model.objects.filter(channel=channel).values(
+                    'user', 'channel', 'date'
+                )
+            )
+            cache.set(key, db_objects, timeout=60)
+            return db_objects
+
+        return cached_result
+
+    def __get_unique_objects(self, objects: list) -> list:
+        # Remove duplicate users and check pending delete
+        unique_objects = []
+        seen_users = set()
+        for combined_object in objects:
+            user = combined_object.get('user')
+            if user not in seen_users:
+                if combined_object.get('pending_delete') is not True:
+                    unique_objects.append(combined_object)
+                seen_users.add(user)
+        return unique_objects
