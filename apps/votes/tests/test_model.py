6ea558b6355efa9df5db5da8efabe35270f6c6fc
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2023-07-30 13:44:51 +0330

    Tests for vote count added

--- apps/votes/tests/test_model.py
+++ apps/votes/tests/test_model.py
@@ -1,18 +1,11 @@
 from django.db.utils import IntegrityError
 import pytest
 from votes.models import Vote
+from channels.models import Channel
 
 
 @pytest.mark.django_db
 class TestVoteModel:
-    def test_delete_votes_after_deleting_video(self, create_vote):
-        """
-        After deleting an objects, its votes should be deleted too.
-        """
-        assert Vote.objects.count() == 1
-        create_vote.content_object.delete()
-        assert Vote.objects.count() == 0
-
     def test_set_channel_for_vote(self, create_vote):
         assert create_vote.channel == create_vote.content_object.channel
 
@@ -29,15 +22,27 @@ class TestVoteModel:
                 content_object=create_vote.content_object
             )
 
-    # TODO: write this tests
-    # def test_get_object_votes_count(self):
-    #     '''test get video votes count'''
-    #     self.create_video()
+    def test_get_object_votes_count_no_vote(self, create_video):
+        """
+        Get count of an objects votes.
+        """
 
-    #     assert cache.get(f"vote_count:{self.video.token}") is None
-    #     assert self.video.get_votes_count()['upvotes'] == 0
+        votes_count = Vote.objects.get_count(
+            content_object=create_video.channel, channel=create_video.channel
+        )
+        assert votes_count == 0
 
-    #     Vote.objects.create(content_object=self.video, user=self.user, choice='u')
-    #     assert cache.get(f"vote_count:{self.video.token}") is not None
-    #     cache.delete(f"vote_count:{self.video.token}")
-    #     assert self.video.get_votes_count()['upvotes'] == 1
+    def test_get_object_votes_count_vote_in_db(self, create_video, create_vote):
+
+        votes_count = Vote.objects.get_count(
+            content_object=create_video.channel, channel=create_video.channel
+        )
+        assert votes_count == 1
+
+    def test_get_object_votes_count_vote_in_cache(self, create_cached_vote):
+        channel = Channel.objects.get(id=create_cached_vote['channel'])
+
+        votes_count = Vote.objects.get_count(
+            content_object=create_cached_vote['content_object'], channel=channel
+        )
+        assert votes_count == 1
