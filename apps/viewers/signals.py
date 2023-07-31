a78593eb0ae7589d92bca20e93e38e1c8f666f8f
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2023-07-31 15:43:08 +0330

    ensure_viewer_exists decorator updated

--- apps/viewers/signals.py
+++ apps/viewers/signals.py
@@ -2,9 +2,10 @@ from viewers.tasks import remove_object_viewers
 from core.utils import get_content_type_model
 
 
-
 def delete_object_viewers_after_deleting(sender, instance, *args, **kwargs):
-    '''Delete an objects viewers that has been saved in db.'''
+    """
+    Deletes all viewers of an object after deleting it.
+    """
 
     remove_object_viewers.delay(
         get_content_type_model(
